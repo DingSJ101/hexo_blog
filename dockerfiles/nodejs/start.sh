6c69ce821c56a0b19b9f96f40a128036b8281899
DingSJ101 <dsj@localhost.localdomain>
2022-06-13 22:55:46 +0800

    fix

--- dockerfiles/nodejs/start.sh
+++ dockerfiles/nodejs/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cd /var/www/blog/blog
+cd /var/www/blog
 
 # 更新代码
 git pull origin master && git submodule foreach git pull origin master
 # git pull origin master
 
+cd blog
 
 # 生成静态资源
 npm install --force

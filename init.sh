c5fefe5bcd45fe6f4e1f4f8c4c569577b504b4a6
DingSJ101 <dsj@localhost.localdomain>
2022-06-14 23:15:17 +0800

    fix dockerfile

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+## start project with one file 
+# cd ~/workspace
+# sudo rm -r hexo_blog
+# mkdir hexo_blog 
+# cd hexo_blog 
+# git init 
+# git config --replace-all --local user.name "DingSJ101" 
+# git config --replace-all  --local user.email "1018966798@qq.com" 
+# git remote add origin https://github.com/DingSJ101/hexo_blog.git
+# git pull origin master
+
 echo "### Stoping containers ..."
 docker-compose down
 

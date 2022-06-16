b8a6efb5273611dc93f3a03c0f45945267bedca9
DingSJ101 <dsj@localhost.localdomain>
2022-06-16 23:04:21 +0800

    fix init.sh

--- init.sh
+++ init.sh
@@ -3,14 +3,17 @@
 # start project with one file 
 cd ~/workspace
 sudo rm -r hexo_blog
-mkdir hexo_blog 
-cd hexo_blog 
-git init 
-# git config --replace-all --local user.name "DingSJ101" 
-# git config --replace-all  --local user.email "1018966798@qq.com" 
-# git remote add origin https://github.com/DingSJ101/hexo_blog.git
-git remote add origin https://gitee.com/dsj_ws/hexo_blog.git
-git pull origin master
+# mkdir hexo_blog 
+# cd hexo_blog 
+# git init 
+# # git config --replace-all --local user.name "DingSJ101" 
+# # git config --replace-all  --local user.email "1018966798@qq.com" 
+# git remote add origin https://gitee.com/dsj_ws/hexo_blog.git
+# git pull origin master
+# git submodule update --init --recursive
+git clone https://gitee.com/dsj_ws/hexo_blog.git hexo_blog
+cd hexo_blog
+git submodule add https://gitee.com/dsj_ws/yilia-plus.git ./blog/theme/yilia-plus
 
 
 echo "### Stoping containers ..."

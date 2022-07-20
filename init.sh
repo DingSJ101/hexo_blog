8389cf7f5a4f94ecbcc27fb9489a270cfa7cc5f8
DingSJ101 <1018966798@qq.com>
2022-07-20 23:14:11 +0800

    >

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # start project with one file 
 cd ~/workspace
-# sudo rm -r hexo_blog
+sudo rm -r hexo_blog
 # mkdir hexo_blog 
 # cd hexo_blog 
 # git init 
@@ -13,7 +13,7 @@ cd ~/workspace
 # git submodule update --init --recursive
 # git clone https://gitee.com/dsj_ws/hexo_blog.git hexo_blog  #部署使用
 # git clone git@gitee.com:starry101/hexo_blog.git  
-# git clone git@github.com:DingSJ101/hexo_blog.git  # 容器开发使用
+git clone git@github.com:DingSJ101/hexo_blog.git  # 容器开发使用
 cd hexo_blog
 
 

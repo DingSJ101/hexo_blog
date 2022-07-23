2ab2e10503d71d2e2d8694dde47af937c39979d7
DingSJ101 <1018966798@qq.com>
2022-07-23 20:49:31 +0800

    fix bug

--- dockerfiles/nodejs/start.sh
+++ dockerfiles/nodejs/start.sh
@@ -2,27 +2,28 @@
 
 cd /var/www/blog
 
-
+sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
 git config --replace-all --global user.name "DingSJ101" 
 git config --replace-all  --global user.email "1018966798@qq.com"
-
-## 更新分支
+ 
+# 更新分支
 git fetch --all
 git reset --hard origin/master
 # 更新代码
 git pull origin master 
-# git submodule add https://gitee.com/starry101/yilia-plus.git ./blog/themes/yilia-plus
-# git submodule add git@gitee.com:starry101/yilia-plus.git ./blog/themes/yilia-plus
-# git rm -r --cached .
+
+# 导入组件
 git submodule init 
-git submodule add -f  https://github.com/next-theme/hexo-theme-next.git  ./themes/next
-git submodule add -f git@gitee.com:starry101/docs.git ./source/_posts
+git submodule add -f  https://github.com/next-theme/hexo-theme-next.git  ./themes/next  # 主题
+git submodule add -f git@gitee.com:starry101/docs.git ./source/_posts   # 文章
 git submodule update --init --recursive
 git submodule foreach git pull origin master
 
+sed -i 's/StrictHostKeyChecking no/#   StrictHostKeyChecking ask/g' /etc/ssh/ssh_config
+
 cd blog
 
-## 插件
+# 插件
 npm install -D md5 moment request xml-parser
 npm install -S hexo-generator-sitemap
 npm install hexo-generator-json-content --save
@@ -31,7 +32,7 @@ npm install hexo-generator-searchdb --save
 npm install hexo-deployer-git --save
 npm install --force
 
-## 生成静态资源
+# 生成静态资源
 hexo clean
 ## hexo-abbrlink 中的 auto category 插件的 title 填充问题（先生成abbrlink，再填充title）
 ## meta中不包含title的文章生成的abbrlink为0
@@ -43,6 +44,6 @@ npm run talk  # 初始化gitalk评论
 # hexo server -p 4000
 
 
-webhook自动部署
+# webhook自动部署
 cd ..
 webhook-cli --port 4000 --hooks hooks.json --verbose
--- init.sh
+++ init.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
 # start project with one file 
+
 cd ~/workspace
 sudo rm -r hexo_blog
-su dsj
 # mkdir hexo_blog 
 # cd hexo_blog 
-# git init 
-# # git config --replace-all --local user.name "DingSJ101" 
-# # git config --replace-all  --local user.email "1018966798@qq.com" 
-# git remote add origin https://gitee.com/dsj_ws/hexo_blog.git
-# git pull origin master
+git config --replace-all --local user.name "DingSJ101" 
+git config --replace-all  --local user.email "1018966798@qq.com" 
 # git submodule update --init --recursive
 # git clone https://gitee.com/dsj_ws/hexo_blog.git hexo_blog  #部署使用
 # git clone git@gitee.com:starry101/hexo_blog.git  
@@ -23,6 +19,8 @@ docker-compose down
 
 echo "### Starting v ..."
 docker-compose -f docker-compose.yml  up --force-recreate --build   
+
+echo "### finish"
 
 
  
